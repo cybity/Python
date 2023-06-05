9866f0bb165e19e93f505e2d5e534f02e593688a
Nishant Singh Tomar <134451804+N1shantST@users.noreply.github.com>
2023-06-05 20:59:35 +0530

    slicing

---
+++ Beginners/list_slicing.py
@@ -0,0 +1,16 @@
+players = ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Frank']
+
+g1 = players[0:2] 
+
+g2 = players[2:4]
+
+g3 = players[4:6]
+
+print("Group 1 :")
+print(g1)
+
+print("Group 2 :")
+print(g2)
+
+print("Group 3 :")
+print(g3)
