91b0ac93eac3bd0948194400f1fcac73881f9c62
Frey <frey0xd@gmail.com>
2023-06-07 00:30:19 +0530

    Function

---
+++ Beginners/STRING_FORMATIIING.py
@@ -0,0 +1,6 @@
+a = "{x}, {y}".format(x=5, y=4)
+
+print(a)
+
+x = ",".join(["frey","AYO"])
+print(x)
---
+++ Beginners/myfun.py
@@ -0,0 +1,18 @@
+def frey():
+    print("Hello")
+    print("Gabimaru")
+
+def exec(word):
+    print(word + "!")
+
+def sum(x,y):
+    print(x + y)
+
+def double(x):
+    print(2 * x)
+
+frey()
+exec("Hello")
+double(8)
+sum(9, 4)
+sum(4, 5)
